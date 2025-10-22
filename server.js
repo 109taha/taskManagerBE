8d4ebb02bad1e0b1434e070410e588a96dbfc937
Taha <11944803+anilhassan512@users.noreply.github.com>
2025-10-23 00:41:49 +0500

    change

--- server.js
+++ server.js
@@ -5,7 +5,15 @@ const connectDB = require("./helper/databaseConnection");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "https://task-manager-fe-five-zeta.vercel.app",
+      "http://localhost:3000",
+    ],
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 // Sample route
